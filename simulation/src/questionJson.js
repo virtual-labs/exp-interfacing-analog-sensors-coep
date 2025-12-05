1f20951a50d4fd6b128ae4721491bcdc97903491
vleadadmin <admin@vlabs.ac.in>
2025-12-05 10:01:28 +0000

    https://virtual-labs.github.io/exp-interfacing-analog-sensors-coep click on the link to test your code.

---
+++ simulation/src/questionJson.js
@@ -0,0 +1,143 @@
+
+
+QuestionsJSON = {
+		"data": {
+			"SEC": [{
+
+				"QUES": [{
+					
+					"QC": "Which temperature sensor works on the principle of change in resistance with temperature?",
+					"ANS": [{
+						"ANSID": true,
+						"content": "RTD (Resistance Temperature Detector)"
+					}, {
+						"ANSID": false,
+						"content": "Thermocouple"
+					}, {
+						"ANSID": false,
+						"content": "IC LM35"
+					},{
+						"ANSID": false,
+						"content": "Infrared Sensor "
+					}]
+				}]
+			}, {
+
+				"QUES": [{
+					
+					"QC": "On which effect a thermocouple works?",
+					"ANS": [{
+						"ANSID": false,
+						"content": "Hall Effect"
+					}, {
+						"ANSID": true,
+						"content": "Seebeck Effect "
+					}, {
+						"ANSID": false,
+						"content": "Photoelectric Effect "
+					}, {
+						"ANSID": false,
+						"content": " Joule Effect"
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": " What is the output of a thermocouple?",
+					"ANS": [{
+						"ANSID": true,
+						"content": " Voltage"
+					}, {
+						"ANSID": false,
+						"content": " Resistance"
+					}, {
+						"ANSID": false,
+						"content": "Current"
+					}, {
+						"ANSID": false,
+						"content": "Frequency"
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": "Which type of temperature sensor provides digital output?",
+					"ANS": [{
+						"ANSID": false,
+						"content": "LM35"
+					}, {
+						"ANSID": true,
+						"content": "DS18B20"
+					}, {
+						"ANSID": false,
+						"content": " Thermistor"
+					}, {
+						"ANSID": false,
+						"content": "RTD"
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": "What happens to the resistance of an NTC Thermister when temperature increases?",
+					"ANS": [{
+						"ANSID": false,
+						"content": "Increases"
+					}, {
+						"ANSID": true,
+						"content": "Decreases"
+					}, {
+						"ANSID": false,
+						"content": "Remains constant"
+					}, {
+						"ANSID": false,
+						"content": "Becomes zero"
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": "Which of the following is a contact-type temperature sensor?",
+					"ANS": [{
+						"ANSID": false,
+						"content": "Infrared sensor"
+					}, {
+						"ANSID": true,
+						"content": "Thermocouple"
+					}, {
+						"ANSID": false,
+						"content": "Fiber Optic sensor"
+					}, {
+						"ANSID": false,
+						"content": "Laser sensor"
+					}]
+					
+				}]
+			}
+			]
+		}
+
+
+	}
+
+
+
+var ANSWERJSON = {
+			
+				"0" : "All the points mentioned",
+				"1" : "Set the standard output for the required limits of process parameter",
+				"2" : "Set majority of the points of the transmitter output in the operating region",
+				"3" : "Pump discharge flow and Outlet flow",
+				"4" : "Nothing is required to be changed",
+				"5" : "Nil"
+				
+			
+}
